f6a9c9d93b0031d6cef90b7a68dc06f7ac75d13d
Learn1 <153932640+masihPemula5@users.noreply.github.com>
2024-03-01 21:26:17 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function tambahkan(value) {
+  document.getElementById('display'
+  ).value += value
+  
+}
+
+function clearDisplay() {
+  document.getElementById('display'
+  ).value = ''
+}
+
+function calculate() {
+  try {
+    let result = eval(document.getElementById('display'
+    ).value)
+    document.getElementById('display'
+    ).value = result
+  } catch (Error) {
+    document.getElementById('display'
+    ).value = 'Error nyet masukin yang bener'
+  }
+}
